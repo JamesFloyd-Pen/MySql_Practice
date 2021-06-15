6104d5bd823661bc9598cb069fcf5da532e9df9e
JamesFloyd-Pen <Commanderpip@gmail.com>
2021-06-15 17:36:06 -0400

    Practice 2

---
+++ World Database/Southern Europe Query.sql
@@ -0,0 +1,8 @@
+select 
+Name, 
+Continent,
+region,
+code
+from country
+where region='Southern Europe'
+order by name ASC;
--- testSQL.sql
+++ testSQL.sql
@@ -1,4 +1,4 @@
-select *
+-- A simple query to print out data from spain in the Katalonia district with above 200,000
+select * 
 from City
 where CountryCode ='ESP' and District = 'Katalonia' and Population >=200000;
-;
